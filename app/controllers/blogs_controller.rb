1d893292e5b8d3097af8e762b15e4ebb8fceaa23
NuWarWarLin <nuwarwarlin.kakehashi2@gmail.com>
2020-08-05 11:00:23 +0630

    add update,delete andconfirm functionally

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,6 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
   def index
     @blogs = Blog.all
   end
@@ -8,23 +10,53 @@ class BlogsController < ApplicationController
   end
 
   def create
-    @blog = Blog.new(blog_params)
-    if @blog.save
-      # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
-      redirect_to blogs_path, notice: "ブログを作成しました！"
-    else
-      # 入力フォームを再描画します。
+    @@blog = Blog.new(blog_params)
+    if params[:back]
       render :new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "Successfully Created Blog!"
+      else
+        render :new
+      end
     end
   end
 
   def show
-    @blog = Blog.find(params[:id])
+    @blog = Blog.find(params[:id])   
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])    
+  end
+
+  def update
+    @blog = Blog.find(params[:id])   
+    
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "Successfully Updated Blog!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice:"Successfully Deleted Blog!"
+  end
+
+  def confirm
+    @blog = Blog.new(blog_params)
+    render :new if @blog.invalid?
   end
 
   private
   def blog_params
-    params.require(:blog).permit(:content)
+    params.require(:blog).permit(:title, :content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,3 @@
 class Blog < ApplicationRecord
-    validates :content, presence: true
+    validates :content, length: {minimum:1, maximum:140}
 end
